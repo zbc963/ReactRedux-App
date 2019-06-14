e10a20f28e616a03d40f587fc41b465150275db0
zbc963 <jingz0629@gmail.com>
2019-06-14 00:03:13 -0700

    section7 finished

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,18 +3,26 @@ import React from 'react';
 
 class SearchBar extends React.Component{
 
-    onInputChange(event){
-        console.log(event.target.value)
+    //event is a javascript object, which is about info of the event that js just occured
+    state = {term:'Hi there'};
+
+
+    onFormSubmit= (event)=>{
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
     }
 
-
+/* onInputChange is callback function, if it is onInputChange(), it will be call whenever the whole component is rendered, without () */
+/* we are passing a reference function to the  input element. Input can call the function when sometime the input is changed */
     render(){
         return(
             <div className="ui segment"> 
-                <form classNmae = "ui form">
+                <form onSubmit={this.onFormSubmit} className = "ui form">
                     <div className="field">
-                        <lable>Image Search</lable>
-                        <input type="text" onChange={this.onInputChange}/>
+                        <label>Image Search</label>
+                        {/* input tag here is uncontrolled event element */}
+                        <input type="text" value={this.state.term} onChange={e=>this.setState({term: e.target.value})}/>
                     </div>
                 </form>
             </div>
